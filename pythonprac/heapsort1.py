a9a76974e4b2d1067e5556f312ed3f2bd994010a
Akash Pandey <akashpracticegfg@gmail.com>
2018-06-03 10:55:20 +0530

    initial commit

---
+++ pythonprac/heapsort1.py
@@ -0,0 +1,9 @@
+"""Implementaion of heapsort from CLRV.
+
+Contains functions to access parent, left-child, right-child of a node in the Max-heap,
+heapify method to create heap out of an unordered array
+"""
+
+def left():
+    """returns the index of left child in the heap representaion of array"""
+
